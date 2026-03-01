bc9e85758676bb1018160d7c93367b496b5b14c0
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2026-03-01 10:51:10 -0400

    feat: create knuckles section layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/home/HeroSection";
+import Knuckles from "@/components/home/Knuckles";
 
 export default function Home() {
   return (
-    <div className="bg-[#1a1b1e] min-h-screen flex items-center justify-center my-10 lg:my-20 ">
+    <div className="bg-[#1a1b1e] min-h-screen flex items-center justify-center my-10 lg:my-30 flex-col gap-25 md:gap-65">
       <HeroSection />
+      <Knuckles />
     </div>
   );
 }
---
+++ components/home/Knuckles.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Knuckles = () => {
+  return (
+    <div className="flex flex-col md:flex-row gap-25 md:gap-10 w-5/6 bg-[#B35A44]/80 py-15 px-5 rounded-3xl items-center justify-around">
+      {/* Texto e imagem do Knuckles */}
+      <div className="flex flex-col items-center">
+        <h2 className="text-white text-2xl md:text-3xl font-bold mb-4 uppercase tracking-wider italic text-center">
+          Esse ano é tudo nosso
+        </h2>
+        <motion.div
+          whileHover={{ scale: 1.02, y: -5 }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image
+            src="/knuckles.svg"
+            alt="Knuckles"
+            className="w-120 "
+            width={200}
+            height={200}
+          />
+        </motion.div>
+        <span className="tracking-wider text-xs uppercase mt-6 font-mono font-bold">
+          (ou use, não sou o seu pai)
+        </span>
+      </div>
+      {/* Imagem do artista */}
+      <div style={{ perspective: "1000px" }}>
+        <motion.div
+          whileHover={{ rotateX: 2, rotateY: -5 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        >
+          {" "}
+          <Image
+            src="/image3.jpg"
+            alt="Murinani Logo"
+            className="w-5/6 mx-auto md:w-80 hover:shadow-[0_0_15px_#1a1b1e] transition-all duration-500 lg:w-100 h-auto rounded-lg object-cover order-1 md:order-0  "
+            width={650}
+            height={750}
+          />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Knuckles;
